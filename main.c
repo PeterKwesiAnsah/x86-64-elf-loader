41bfb3fb83028b0dce029179b61790b9067ef74d
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2026-02-12 00:23:26 +0000

    feat: ET-PIE loading

--- main.c
+++ main.c
@@ -36,43 +36,63 @@ int LoadET(int fd, size_t page_size, char **interpath)
   }
 
   Elf64_Ehdr *ehdr = (Elf64_Ehdr *)addr;
-  __uint8_t *baddr = (__uint8_t *)0;
   Elf64_Phdr *pht_start = (Elf64_Phdr *)(addr + ehdr->e_phoff);
-
   int pht_i = 1;
-  // TODO: change the access protections
+  Elf64_Addr max_vaddr = 0;
+  Elf64_Addr min_vaddr = 0;
+  size_t loadsegmmap_len = 0;
+  int loagseg_i[__UINT8_MAX__] = {0};
   for (; pht_i < ehdr->e_phnum; pht_i++)
   {
     if (pht_start[pht_i].p_type == PT_INTERP)
     {
-      *interpath = (char *)addr + pht_start[pht_i].p_offset;
+      //
     }
     else if (pht_start[pht_i].p_type == PT_LOAD)
     {
-      //These file page segments automatically have corresponding memory page-bounded segments
-      if (addr + pht_start[pht_i].p_offset % page_size==0)
+      if (pht_start[pht_i].p_vaddr > max_vaddr)
       {
-        int status = mprotect(addr + pht_start[pht_i].p_offset, pht_start[pht_i].p_filesz, elf_pflags_to_mmap_prot((int)pht_start[pht_i].p_flags));
-        if (status < 0)
-        {
-          perror("mprotect failed");
-          return 1;
-        }     
+        max_vaddr = pht_start[pht_i].p_vaddr;
+        loadsegmmap_len = pht_start[pht_i].p_vaddr + pht_start[pht_i].p_memsz;
+        loagseg_i[pht_i] = pht_i;
       }
-      //While these file page segments are relative to page-aligned file segments as thier memory counter parts too
-      else
-      {
-        Elf64_Off relap_offset= pht_start[pht_i].p_offset & ~(page_size - 1);
-        Elf64_Addr relap_vadress=pht_start[pht_i].p_vaddr & ~(page_size - 1);
-      }
+      else if (pht_start[pht_i].p_vaddr <= min_vaddr)
+        min_vaddr = pht_start[pht_i].p_vaddr;
     }
+  }
+  pht_i = 1;
+  // now we reserve region
+  __uint8_t *segs_addr = mmap(NULL, loadsegmmap_len, PROT_NONE, MAP_PRIVATE, -1, 0);
+  if (addr == MAP_FAILED)
+  {
+    perror("mmap failed");
+    return 1;
+  }
+  __uint8_t *baddr = segs_addr - min_vaddr;
+  for (; pht_i < __UINT8_MAX__; pht_i++)
+  {
+    if (loagseg_i[pht_i] == 0)
+      continue;
 
-    pht_i++;
-  };
-  // close(fd);
+    Elf64_Off relap_offset = pht_start[pht_i].p_offset & ~(page_size - 1);
+    Elf64_Addr relap_vadress = pht_start[pht_i].p_vaddr & ~(page_size - 1);
+    mmap(baddr + relap_vadress, pht_start[pht_i].p_filesz, elf_pflags_to_mmap_prot(pht_start[pht_i].p_flags), MAP_PRIVATE | MAP_FIXED, fd, relap_offset);
+    {
+      perror("mmap failed");
+      return 1;
+    }
+    if (pht_start[pht_i].p_memsz > pht_start[pht_i].p_filesz)
+    {
+      size_t bss_size = pht_start[pht_i].p_memsz - pht_start[pht_i].p_filesz;
+      memset(baddr + pht_start[pht_i].p_offset + pht_start[pht_i].p_filesz, '\0', bss_size);
+    }
+  }
+
+  close(fd);
+  munmap(addr, fsize);
   // setup stack
   // setup a memory image for program interpretor
-  //  jump to _start
+  // jump to _start
   return 0;
 }
 
